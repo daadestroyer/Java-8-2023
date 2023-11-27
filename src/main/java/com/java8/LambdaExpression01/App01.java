53f392241949fec551aae8e8c2e9a00a195c7888
Shubham Nigam <48306820+daadestroyer@users.noreply.github.com>
2023-11-27 21:21:26 +0530

    first commit

---
+++ src/main/java/com/java8/LambdaExpression01/App01.java
@@ -0,0 +1,13 @@
+package com.java8.LambdaExpression01;
+/*
+Functional Interface:
+---------------------
+Functional Interface are those
+*/
+@FunctionalInterface
+
+public class App01 {
+    public static void main(String[] args) {
+
+    }
+}
